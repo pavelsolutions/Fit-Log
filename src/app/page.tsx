9f08d0ce9693610fe3f67987fbfaa846f1f8cbac
Pavel Ahammed <pahammed69@gmail.com>
2026-09-23 21:37:25 +0600

    Tailwind CSS & DaisyUI Setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div>
-      <h1>Home Page</h1>
+      <h1 className="bg-amber-300">Home Page</h1>
     </div>
   );
 }
